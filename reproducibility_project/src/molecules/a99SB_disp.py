1402109136e96785da188ef19dadb2eb6e3f3556
GregorySchwing <gschwing@uno.edu>
2022-03-25 19:07:58 -0500

    Added opc3, lowercased my filenames, added python builders for the 4 points

---
+++ reproducibility_project/src/molecules/a99SB_disp.py
@@ -0,0 +1,25 @@
+"""Create united-atom representation of water."""
+
+import mbuild as mb
+
+
+class a99SB_disp(mb.Compound):
+    """Create a single particle water compound."""
+
+    def __init__(self):
+        super(a99SB_disp, self).__init__()
+        abs_path = os.path.dirname(os.path.abspath(molecules.__file__))
+        self.add(mb.load(f"{abs_path}/a99SB_disp.mol2"), label="WAT")
+
+
+def main():
+    """Create a a99SB_disp compound and print basic properties."""
+    water = a99SB_disp()
+    print(water)
+    print(water.name)
+    print(water.labels)
+    print(water["WAT"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ reproducibility_project/src/molecules/tip4p_ew.py
@@ -0,0 +1,25 @@
+"""Create united-atom representation of water."""
+
+import mbuild as mb
+
+
+class tip4p_ew(mb.Compound):
+    """Create a single particle water compound."""
+
+    def __init__(self):
+        super(tip4p_ew, self).__init__()
+        abs_path = os.path.dirname(os.path.abspath(molecules.__file__))
+        self.add(mb.load(f"{abs_path}/tip4p_ew.mol2"), label="WAT")
+
+
+def main():
+    """Create a tip4p_ew compound and print basic properties."""
+    water = tip4p_ew()
+    print(water)
+    print(water.name)
+    print(water.labels)
+    print(water["WAT"])
+
+
+if __name__ == "__main__":
+    main()
