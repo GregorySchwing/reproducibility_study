24e2921cb25829e065626a92aa6e9596e9ef9e23
GregorySchwing <gschwing@uno.edu>
2022-04-10 03:52:04 -0500

    print true extend and cionize extent

--- reproducibility_project/src/proteins/ionizeTemplate.tcl
+++ reproducibility_project/src/proteins/ionizeTemplate.tcl
@@ -11,4 +11,8 @@ puts "$numCatAn"
 set solute [mol new PATH_2_PROT_PSF waitfor all]
 mol addfile PATH_2_PROT_PDB mol $solute waitfor all
 
-cionize -mol 0 -np 4 -mg -prefix default -ions "{CATION_NAME [lindex $numCatAn 0] CATION_VAL} {ANION_NAME [lindex $numCatAn 1] ANION_VAL}" 
+set all [atomselect $solute all] 
+set minmax [measure minmax $all]
+set vals "[lindex [lindex $minmax 0] 0] [lindex [lindex $minmax 0] 1] [lindex [lindex $minmax 0] 2] [lindex [lindex $minmax 1] 0] [lindex [lindex $minmax 1] 1] [lindex [lindex $minmax 1] 2]"
+puts "$vals"
+cionize -mol 0 -np 4 -mg -prefix default -border 0 -ga test -ions "{CATION_NAME [lindex $numCatAn 0] CATION_VAL} {ANION_NAME [lindex $numCatAn 1] ANION_VAL}" 
