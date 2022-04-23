041d3f9fdd6b2d31cf088e264210f4a3a34bd3ba
Gregory J. Schwing <go2432@wayne.edu>
2022-04-23 15:40:24 -0400

    wrote launch script to init and do first submission on grid

---
+++ reproducibility_project/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Example with 28 cores for OpenMP
+#
+# Project/Account
+#SBATCH --qos=secondary
+# Request one node
+#SBATCH -N 1
+# Total number of cores, in this example it will 1 node with 1 core each. 
+#SBATCH -n 1
+#SBATCH --mem=5G
+#
+# Runtime of this jobs is less then 12 hours.
+#SBATCH --time=0-0:01:00
+#
+#SBATCH --mail-user=go2432@wayne.edu
+
+#SBATCH -o output_%j.out
+
+#SBATCH -e errors_%j.err
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate mosdef-study38
+export PYTHONPATH=$PYTHONPATH:wsu/home/go/go24/go2432/reproducibility_study/reproducibility_project
+python init.py
+python src/engines/gomc/project.py submit
