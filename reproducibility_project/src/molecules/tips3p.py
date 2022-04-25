3600683a98cce3c4724625a51772121126f587ec
GregorySchwing <gschwing@uno.edu>
2022-04-24 23:40:14 -0500

    rename

--- reproducibility_project/src/molecules/tips3p.py
+++ reproducibility_project/src/molecules/tips3p.py
@@ -6,18 +6,18 @@ import mbuild as mb
 from reproducibility_project.src import molecules
 
 
-class tip3p(mb.Compound):
+class tips3p(mb.Compound):
     """Create a single particle water compound."""
 
     def __init__(self):
-        super(tip3p, self).__init__()
+        super(tips3p, self).__init__()
         abs_path = os.path.dirname(os.path.abspath(molecules.__file__))
         self.add(mb.load(f"{abs_path}/tips3p.mol2"), label="WAT")
 
 
 def main():
-    """Create a tip3p compound and print basic properties."""
-    water = tip3p()
+    """Create a tisp3p compound and print basic properties."""
+    water = tips3p()
     print(water)
     print(water.name)
     print(water.labels)
