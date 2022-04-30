fa6d334f1c712ee00b0a3c5cdb0ad95ac0cb56df
GregorySchwing <gschwing@uno.edu>
2022-04-30 01:50:47 -0500

    l

--- reproducibility_project/launch.sh
+++ reproducibility_project/launch.sh
@@ -2,25 +2,21 @@
 # Example with 28 cores for OpenMP
 #
 # Project/Account
-#SBATCH --qos=primary
+#SBATCH --qos=gpu
 # Request one node
 #SBATCH -N 1
 # Total number of cores, in this example it will 1 node with 1 core each. 
-#SBATCH -n 1
-#SBATCH --mem=5G
-#
+#SBATCH -n 5
+#SBATCH --mem=12G
+#SBATCH --cpus-per-task=2
 # Runtime of this jobs is less then 12 hours.
-#SBATCH --time=0-1:00:00
-#
+#SBATCH --time=7-0:00:00
+#SBATCH --pty /bin/bash
 #SBATCH --mail-user=go2432@wayne.edu
 
 #SBATCH -o output_%j.out
 
 #SBATCH -e errors_%j.err
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate mosdef-study38
-export PYTHONPATH=$PYTHONPATH:wsu/home/go/go24/go2432/reproducibility_study/reproducibility_project
-python init.py
-signac view
-python src/engines/gomc/project.py status
-python src/engines/gomc/project.py submit
+source /sw/packages/python-tools/anaconda3/etc/profile.d/conda.sh
+conda activate mamba
+python src/engines/namd/project.py run
