f1a0870977c98a6ce6c4139d01205c02ecf1dd6c
greg <go2432@wayne.edu>
2022-04-30 07:33:30 +0000

    :A:

--- reproducibility_project/launch.sh
+++ reproducibility_project/launch.sh
@@ -9,6 +9,7 @@
 #SBATCH -n 5
 #SBATCH --mem=12G
 #SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
 # Runtime of this jobs is less then 12 hours.
 #SBATCH --time=4:00:00
 #SBATCH --mail-user=go2432@wayne.edu
@@ -17,5 +18,9 @@
 
 #SBATCH -e errors_%j.err
 source /sw/packages/python-tools/anaconda3/etc/profile.d/conda.sh
+export PYTHONPATH=$PYTHONPATH:/home/udpk5hr/reproducibility_study/reproducibility_project/:/home/udpk5hr/reproducibility_study
 conda activate mamba
+module load cuda
+nvidia-smi
 python src/engines/namd/project.py run
+
