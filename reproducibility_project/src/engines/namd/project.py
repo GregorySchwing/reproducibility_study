a06bb7354e42686348dfab4cb53f26c0bc4a9b1e
greg <go2432@wayne.edu>
2022-06-03 18:42:52 -0500

    c

--- reproducibility_project/src/engines/namd/project.py
+++ reproducibility_project/src/engines/namd/project.py
@@ -484,7 +484,7 @@ def part_2a_solvated(job):
     saltless_sp['salt_conc']=None
     saltless_sp['replica']=0
     #print("statepoint desalted",saltless_sp)
-    for job in project.find_jobs(saltless_sp):
+    for job in Project.find_jobs(saltless_sp):
         if job.isfile(f"{'solvated.pdb'}"):
             data_written_bool = True
 
