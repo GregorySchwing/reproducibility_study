9989ab7bd854ad797b686159f64a3cddfdcfe88f
greg <go2432@wayne.edu>
2022-06-03 19:58:20 -0500

    l

--- reproducibility_project/src/engines/namd/project.py
+++ reproducibility_project/src/engines/namd/project.py
@@ -481,7 +481,7 @@ def part_2a_solvated(job):
     """Check that the initial job data is written to the json files."""
     data_written_bool = False
     print(Project)
-    print(get_id())
+    print(signac.get_id())
     #signac.Project.find_jobs({'replica': 0}).export_to('foo_0.tar')
 
     #Project.open_job({'salt_conc': None, 'replica': 0, 'molecule': job.sp.molecule, 'pdbid': job.sp.pdbid}).init()
