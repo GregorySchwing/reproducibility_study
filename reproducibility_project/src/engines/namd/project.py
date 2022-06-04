b24c388fc2d59681b62e032fb5268810ff4ff1b5
greg <go2432@wayne.edu>
2022-06-03 19:26:48 -0500

    t

--- reproducibility_project/src/engines/namd/project.py
+++ reproducibility_project/src/engines/namd/project.py
@@ -487,7 +487,7 @@ def part_2a_solvated(job):
     for key, val in zip(saltless_sp.keys(), saltless_sp.values()):
         if (val == None):
             mappingStrign+= (f'\"{key}\": null,')
-        elif val.isnumeric():
+        elif str(val).isnumeric():
             mappingStrign+= (f'\"{key}\": {val},')
         else:
             mappingStrign+= (f'\"{key}\": "{val}",')
