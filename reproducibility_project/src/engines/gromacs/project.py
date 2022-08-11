6c74d1019d745c832e2983cf4af383ac85c98c2f
greg <go2432@wayne.edu>
2022-08-10 20:13:51 -0500

    a

--- reproducibility_project/src/engines/gromacs/project.py
+++ reproducibility_project/src/engines/gromacs/project.py
@@ -103,12 +103,19 @@ def init_job(job):
 
     try:
         os.symlink(get_ff_path(job.sp.forcefield_name), ".")
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            print('Directory not created.')
+        else:
+            raise
+    try:
         os.symlink(get_wm_path(job.sp.forcefield_name), ".")
     except OSError as e:
         if e.errno == errno.EEXIST:
             print('Directory not created.')
         else:
             raise
+
     os.system(pdb2gmxCommand)  # 2.25 Å Resolution
 
 
